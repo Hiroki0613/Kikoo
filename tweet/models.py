88bf56d6e6c7ba231e5274d172ee1ef8f2f083eb
Hiroki Kondo <kondo.matr02@gmail.com>
2020-06-21 23:18:35 +0900

    tweet画面の実装開始

--- tweet/models.py
+++ tweet/models.py
@@ -7,3 +7,8 @@ class Post(models.Model):
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     date_posted = models.DateTimeField(default=timezone.now)
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.content
